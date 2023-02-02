eb94c9e5b46c0906e2689fcea72485db5788816a
ansh00sharma <sharma999ansh@gmail.com>
2023-02-02 13:01:45 +0530

    initial commit

---
+++ src/Components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import './NewExpense.css';
+import NewExpenseForm from './NewExpenseForm';
+
+function NewExpense(){
+
+    return (
+        <div className='new-expense'>
+            <NewExpenseForm/>
+        </div>
+    );
+}
+
+export default NewExpense
